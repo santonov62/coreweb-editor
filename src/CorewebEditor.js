08887332c417c2cb6e6edac3ea9297778c2f94e8
mpashnikov <mpashnikov@nymbus.com>
2021-02-05 00:16:34 +0300

    init version

--- src/CorewebEditor.js
+++ src/CorewebEditor.js
@@ -1,15 +1,39 @@
 import { LitElement, html, css } from 'lit-element';
-import { openWcLogo } from './open-wc-logo.js';
 
 export class CorewebEditor extends LitElement {
   static get properties() {
     return {
-      title: { type: String },
+      title: {type: String},
+      itemsCount: {type: Number},
+      columnsCount: {type: Number}
     };
   }
 
+  static containerStyles = css`.container {
+        display: grid;
+        position: relative;
+        padding: 10px;
+        height: 100%;
+        width: 100%;
+        gap: 10px;
+        place-items: stretch;
+        min-height: 400px;
+      }
+
+      .container div {
+        background: #3273dc;
+        color: white;
+        padding: 20px;
+        font-size: 21px;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+  `;
+
   static get styles() {
-    return css`
+    return [css`
       :host {
         min-height: 100vh;
         display: flex;
@@ -23,67 +47,61 @@ export class CorewebEditor extends LitElement {
         text-align: center;
         background-color: var(--coreweb-editor-background-color);
       }
-
-      main {
-        flex-grow: 1;
-      }
-
-      .logo > svg {
-        margin-top: 36px;
-        animation: app-logo-spin infinite 20s linear;
-      }
-
-      @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
-      }
-
-      .app-footer a {
-        margin-left: 5px;
-      }
-    `;
+    `, this.containerStyles];
   }
 
   constructor() {
     super();
     this.title = 'My app';
+    this.itemsCount = 1;
+    this.columnsCount = 1;
+  }
+
+  #getColumnsTemplateStr() {
+    let res = 'grid-template-columns: 1fr';
+    for (let i=0; i<this.columnsCount-1; i++) {
+      res += ' 1fr';
+    }
+    return res;
+  }
+
+  addColumn() {
+    this.columnsCount++;
+  }
+
+  deleteColumn() {
+    if (this.columnsCount > 0)
+      this.columnsCount--;
+  }
+
+  #getItemTemlates() {
+    const itemTemplates = [];
+    for (let i=0; i<this.itemsCount; i++) {
+      itemTemplates.push(html`<div data-fieldname="name">${i+1}</div>`);
+    }
+    return itemTemplates;
+  }
+
+  addItem() {
+    this.itemsCount++;
+  }
+
+  deleteItem() {
+    if (this.itemsCount > 0)
+      this.itemsCount--;
+  }
+
+  getHTMLTemplate() {
+    return CorewebEditor.containerStyles + '\n' + this.shadowRoot.querySelector('.container').outerHTML;
   }
 
   render() {
     return html`
-      <main>
-        <div class="logo">${openWcLogo}</div>
-        <h1>${this.title}</h1>
-
-        <p>Edit <code>src/CorewebEditor.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      <div class="container" style="${this.#getColumnsTemplateStr()}">
+        ${this.#getItemTemlates()}
+      </div>
     `;
   }
 }
+
+customElements.define('coreweb-editor', CorewebEditor);
